7f9e9c706f19a1cc584c61086e8f3c65fb917baf
Byron Gamboa <byrongamboa@outlook.com>
2020-04-24 21:18:33 -0500

    Clase 2 - Variables: Strings

---
+++ clase2.js
@@ -0,0 +1,12 @@
+var nombre = 'Byron';
+var apellido = 'Gamboa';
+
+
+var nombreEnMayuscula = nombre.toLocaleUpperCase();
+var apellidoEnMinuscula = apellido.toLocaleLowerCase();
+var cantidadDeLetrasEnNombre = nombre.length;
+var primeraLetraEnNombre = nombre.charAt(0);
+
+var nombreCompleto =`${nombre} ${apellido.toLocaleUpperCase}`;
+
+var str = nombre.substr(1,2);
